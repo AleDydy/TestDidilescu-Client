97351133ea9887b52d7818dc0fe7c7d2d0755812
lai2-24 <lai2-24@itismeucci.com>
2023-11-07 13:40:01 +0100

    commit finale con il codice completato

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,13 +1,73 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+        try {
+            Socket s = new Socket("localhost", 3000);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+            System.out.println("Benvenuto alla biglietteria! Inserisci D per verificare la presenza di biglietti, A per acquistare un biglietto e Q per uscire");
+
+            Scanner input = new Scanner(System.in);
+
+            String messfromuser;
+            String messfromserver = "L";
+            boolean exit=true;
+
+            while(exit==true){
+                messfromuser = input.nextLine();
+                out.writeBytes(messfromuser + "\n");
+
+                messfromserver = in.readLine();
+
+                if(messfromserver.equals("5.")){
+                    System.out.println("Biglietti disponibili: 5");
+                }
+                if(messfromserver.equals("4.")){
+                    System.out.println("Biglietti disponibili: 4");
+                }
+                if(messfromserver.equals("3.")){
+                    System.out.println("Biglietti disponibili: 3");
+                }
+                if(messfromserver.equals("2.")){
+                    System.out.println("Biglietti disponibili: 2");
+                }
+                if(messfromserver.equals("1.")){
+                    System.out.println("Biglietti disponibili: 1");
+                }
+                if (messfromserver.equals("@")) {
+                    System.out.println("Biglietto acquistato con successo");
+                }
+                if(messfromserver.equals(".")){
+                    exit=false;
+                }
+                if(messfromserver.equals("..")){
+                    exit=false;
+                }
+
+            }
+
+            if(messfromserver.equals(".")){
+                System.out.println("I biglietti sono esauriti");
+            }
+            
+            s.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
